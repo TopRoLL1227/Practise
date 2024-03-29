a4ad1f98314e1d097b99e24dc3db3f0d645f917d
Volodymyr <terces@ukr.net>
2024-03-29 19:32:10 +0200

    next

--- APP/urls.py
+++ APP/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:describes_zodiac>/', views.get_describe_zodiac_by_number),
-    path('<str:describes_zodiac>/', views.get_describe_zodiac),
+    path('<str:describes_zodiac>/', views.get_describe_zodiac, name='name1'),
 ]
--- APP/views.py
+++ APP/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 zodiac_dict = {
     'leo': 'Лев',
-    'cancer': 'Рак'
+    'cancer': 'Рак',
 
 }
 
@@ -23,4 +24,5 @@ def get_describe_zodiac_by_number(request, describes_zodiac):
     if describes_zodiac > len(zodiac_list):
         return HttpResponseNotFound(f'Not Found')
     zodiac = zodiac_list[describes_zodiac - 1]
-    return HttpResponseRedirect(f'/horoscope/{zodiac}')
+    redirect_url = reverse('name1', args=[zodiac])
+    return HttpResponseRedirect(redirect_url)
