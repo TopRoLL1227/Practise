5386c5ea3a8caa189b0845c7d6033898c1bdf4ed
Volodymyr <terces@ukr.net>
2024-04-04 12:16:13 +0300

    04.04.2024

--- APP/views.py
+++ APP/views.py
@@ -11,12 +11,20 @@ zodiac_dict = {
 }
 
 
-def get_describe_zodiac(request, describes_zodiac):
-    description = zodiac_dict.get(describes_zodiac, None)
-    if description:
-        return HttpResponse(description)
-    else:
-        return HttpResponseNotFound(f"Невідомий - {describes_zodiac}")
+# def get_describe_zodiac(request, describes_zodiac):
+#     description = zodiac_dict.get(describes_zodiac, None)
+#     if description:
+#         return HttpResponse(description)
+#     else:
+#         return HttpResponseNotFound(f"Невідомий - {describes_zodiac}")
+
+def get_describe_zodiac(request, describes_zodiac: str):
+    rdy_describe_dict = zodiac_dict.get(describes_zodiac)
+    data = {
+        'describes': rdy_describe_dict,
+        'title': describes_zodiac,
+    }
+    return render(request, 'APP/info.html', context=data)
 
 
 def get_describe_zodiac_by_number(request, describes_zodiac):
@@ -26,3 +34,25 @@ def get_describe_zodiac_by_number(request, describes_zodiac):
     zodiac = zodiac_list[describes_zodiac - 1]
     redirect_url = reverse('name1', args=[zodiac])
     return HttpResponseRedirect(redirect_url)
+
+
+# def index(request):
+#     description = list(zodiac_dict)
+#     res = ''
+#     for x in description:
+#         redirect_path = reverse('name1', args=[x])
+#         res += f"<li><a href={redirect_path}> {x} </a> </li>"
+#     result = f"""
+#         <ol>
+#             {res}
+#         </ol>
+#     """
+#     return HttpResponse(result)
+
+
+def index(request):
+    description = list(zodiac_dict)
+    context = {
+        'description': description
+    }
+    return render(request, 'APP/index.html', context=context)
